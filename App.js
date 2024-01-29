b08c1a79117027d73646eeb50649e8518ad18e65
SoniKetan <151501504+SoniKetan@users.noreply.github.com>
2024-01-29 22:48:28 +0530

    001--expense_tracker

---
+++ App.js
@@ -0,0 +1,11 @@
+import Items from "./Components/Items";
+
+function App() {
+  return <div>
+    <h1> Expense Items </h1>
+    <Items></Items>
+  </div>
+
+}
+
+export default App;
---
+++ Items.jsx
@@ -0,0 +1,9 @@
+function Items() {
+    return <div>
+        <div>Food Rs 10</div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+    </div>
+}
+
+export default Items;
